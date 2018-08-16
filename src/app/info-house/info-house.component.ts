fc97ea07016a5303d4f396119d4561f0a16b85d1
Aleksei-S <mz-43@mail.ru>
2018-08-16 13:00:54 +0300

    16.08

--- src/app/info-house/info-house.component.ts
+++ src/app/info-house/info-house.component.ts
@@ -21,7 +21,7 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
   @ViewChild('layerLand') layerLand: KonvaComponent;
   @ViewChild('layerWater') layerWater: KonvaComponent;
   @ViewChild('layerfreeDraw') layerfreeDraw: KonvaComponent;
-
+  @ViewChild('layerLevel_0') layerLevel_0: KonvaComponent;
 
 
   public width = 400;
@@ -34,17 +34,7 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
   private maxLevel_water = 0;
 
 
-  // level_0: ['', []],
-  // level_ground: ['', []],
-  // level_pit: ['', []],
-  // level_water: ['', []],
-  // typeFoundation: ['', []],
-  // // dateTimeEvent: [this.dateNowISO, [Validators.required, this.dataValidator]],
-  // numberStoreys: ['', []],
-  // numberSection: ['', []],
-  // S_apartments: ['', []],
-  // S_loft: ['', []],
-  // S_basement: ['', []],
+
 
 
 
@@ -73,37 +63,34 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     const layer = this.layerfreeDraw.getStage();
     const stage = this.stage.getStage();
-   
-    const isPaint = false;
+    let isPaint = false;
     let line: any;
 
-    stage.on('mouseup', function(e) {
-      // console.log('mouseup');
-      this.isPaint = false;
-      this.line.points(this.line.points().concat([stage.getPointerPosition().x, stage.getPointerPosition().y]));
+    stage.on('mouseup', (e) => {
+      if (!isPaint) { return; }
+      line.points(line.points().concat([stage.getPointerPosition().x, stage.getPointerPosition().y]));
       layer.draw();
+      isPaint = false;
     });
-    stage.on('mousedown', function(e) {
-      // console.log('mousedown');
-      this.isPaint = true;
-      this.line = new Konva.Line({
-        points:[stage.getPointerPosition().x, stage.getPointerPosition().y],
-        stroke: '#df4b26', //black
-        strokeStyle: "#df4b26",
-        lineJoin: "round",
-        lineWidth: 5,
-        strokeWidth: 3,
-        lineCap: "round",
-      });
-      layer.add(this.line);
+
+    stage.on('mousedown', (e) => {
+      if (e.target.nodeType !== 'Stage') { return; }
+        isPaint = true;
+        line = new Konva.Line({
+          points: [stage.getPointerPosition().x, stage.getPointerPosition().y],
+          stroke: 'black',
+          strokeStyle: '#df4b26',
+          lineJoin: 'round',
+          lineWidth: 5,
+          strokeWidth: 3,
+          lineCap: 'round',
+        });
+        layer.add(line);
     });
-      stage.on('mousemove', function(e) {
-        if (!this.isPaint) {
-          return;
-        }
-        this.line.points(this.line.points().concat([stage.getPointerPosition().x, stage.getPointerPosition().y]));
-        console.log(layer);  
-        layer.draw();
+      stage.on('mousemove', (e) => {
+        if (!isPaint) { return; }
+          line.points(line.points().concat([stage.getPointerPosition().x, stage.getPointerPosition().y]));
+          layer.draw();
       });
 
 
@@ -191,16 +178,8 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
     });
   }
 
-  changeLevel_0(e) {
-    // console.log('changeLevel_0 ' + e);
-    e = e.replace(',', '.');
-    this.level_0 = parseFloat(e);
-    // console.log(this.level_0);
-    if (this.level_0 && this.level_ground_middle) {
-      console.log(this.level_0);
-    }
-  }
-  takeMiddlFromArray(arr:any[]){
+
+  takeMiddlFromArray(arr: any[]) {
     let count = 0;
     arr.forEach(e => {
       count = count + parseFloat(e.replace(',', '.'));
@@ -208,27 +187,31 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
     return count / (arr.length);
   }
 
+  changeLevel_0(e) {
+    e = e.replace(',', '.');
+    this.level_0 = parseFloat(e);
+    this.checkDraw();
+  }
+
   changeLevel_ground(e) {
     const levelGroundArr = e.match(/\d*[.,]?\d+/gm);
-    if (!levelGroundArr) return;
+    if (!levelGroundArr) { return; }
     this.level_ground_middle = this.takeMiddlFromArray(levelGroundArr);
-    console.log(this.level_ground_middle);
+    this.checkDraw();
   }
 
   changeLevel_pit(e) {
-    console.log('changeLevel_pit ' + e);
     const level_pitArr = e.match(/\d*[.,]?\d+/gm);
-    if (!level_pitArr) return;
+    if (!level_pitArr) { return; }
     this.level_pit_middle = this.takeMiddlFromArray(level_pitArr);
     this.checkDraw();
-    console.log(this.level_pit_middle);
   }
   changeLevel_water(e) {
     const arrWarter = e.match(/\d*[.,]?\d+/gm);
     if (!arrWarter) {
       return;
     }
-    let maxLevel_water = 0;
+    const maxLevel_water = 0;
     const arr = arrWarter.map(element => {
       element = element.replace(',', '.');
       element = parseFloat(element);
@@ -244,8 +227,8 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
 
     if (!this.level_ground_middle) {
       return;
-    } 
-    
+    } else
+
     if (this.maxLevel_water) {
       const layer = this.layerWater.getStage();
       layer.destroyChildren();
@@ -255,13 +238,11 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
       const group = new Konva.Group({
         x: this.width * 0.8,
         y: this.height * 0.4 + h * this.coef,
-        // draggable: true
       });
       const mark = this.basicInfoService.drawMark(1, this.maxLevel_water.toFixed(3));
       group.add(new Konva.Line(mark.arrow), new Konva.Line(mark.arrowEnd), new Konva.Text(mark.text));
       layer.add(group);
       layer.draw();
-   
     }
 
     if (this.level_pit_middle && this.level_0) {
@@ -271,6 +252,17 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
       const h = this.level_ground_middle - (this.level_0 - this.level_pit_middle);
       const land = this.basicInfoService.drawLand(this.width, this.height, this.coef, h);
       layer.add(new Konva.Line(land));
+      {
+
+        const group2 = new Konva.Group({
+          x: this.width * 0.8,
+          y: this.height * 0.4,
+        });
+        const mark2 = this.basicInfoService.drawMark(1, this.level_ground_middle.toFixed(3));
+        group2.add(new Konva.Line(mark2.arrow), new Konva.Line(mark2.arrowEnd), new Konva.Text(mark2.text));
+        layer.add(group2);
+        layer.draw();
+      }
       const group = new Konva.Group({
         x: this.width * 0.8,
         y: this.height * 0.4 + h * this.coef,
@@ -288,19 +280,13 @@ export class InfoHouseComponent implements OnInit, AfterViewInit {
   
     }
 
-    // else if (!this.level_ground_middle) {
-
-    // } else if (!this.maxLevel_water) {
-
-    // }
-
   // private level_0 = 0;
   // private level_ground_middle = 0;
   // private level_pit_middle = 0;
   // private level_water_middle = 0;
   }
 
-
+  drawLandCheck() {}
 
 
 
